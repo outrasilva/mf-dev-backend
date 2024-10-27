00c7c57d26fc0392b52df3c0e12de1ec37453e3e
outrasilva <jessica.1702@hotmail.com>
2024-10-27 11:09:49 -0300

    Instalação de Pacotes e Conexão com o BD.

---
+++ mf-dev-backend/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
